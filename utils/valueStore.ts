e7beea98b37dbf3a8dbe156c4ec8b9a2823ffda9
furkandmrblk <ibrahimdmrblk@outlook.de>
2021-10-22 10:43:08 +0200

    🎨  - qrcode util

--- utils/valueStore.ts
+++ utils/valueStore.ts
@@ -1,5 +1,6 @@
 // Library Utilities
 
+import deviceStorage from '../providers/deviceStorage';
 import { LibraryProps } from './types';
 
 let library: LibraryProps | undefined = undefined;
@@ -26,4 +27,8 @@ export const getFloor = () => {
   return floor;
 };
 
-// Table Utilities
+// QRCode Utilities
+
+export const getIdentifier = async () => {
+  return await deviceStorage.get('tableIdentifier');
+};
