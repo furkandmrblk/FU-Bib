ff36ee7c4fca6dfac3c05654c34943805492e14c
furkandmrblk <ibrahimdmrblk@outlook.de>
2021-10-22 10:42:09 +0200

    ✨  - qrcode tab

--- screens/TabThreeScreen.tsx
+++ screens/TabThreeScreen.tsx
@@ -1,27 +1,101 @@
 import { useIsFocused } from '@react-navigation/core';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { UpperBody } from '../components/Body/UpperBody';
 import Button from '../components/Button/Button';
 import { Header } from '../components/Header/Header';
-import { bodyContainerStyle } from '../components/Libraries/Library';
 import { ManropeText } from '../components/StyledText';
 import { View } from '../components/Themed';
-import { purple100, white } from '../constants/Colors';
-import { getLibrary } from '../utils/valueStore';
+import { white } from '../constants/Colors';
+import { getIdentifier, getLibrary } from '../utils/valueStore';
 import { containerStyle, headerTitleStyle } from './TabOneScreen';
 import QRCodeIcon from '../assets/images/QRCodeIcon';
 import { RootTabScreenProps } from '../types';
+import { gql, useQuery } from '@apollo/client';
+import { QRContainer } from '../components/QR/QRContainer';
 
-const hasQR: boolean = false;
+const getTable = gql`
+  query getTable($identifier: String!) {
+    getTable(identifier: $identifier) {
+      id
+      identifier
+      library {
+        name
+        adress
+        email
+        website
+      }
+      floor
+      userId
+      time
+    }
+  }
+`;
+
+export const getCurrentUser = gql`
+  query getCurrentUser {
+    getCurrentUser {
+      id
+      name
+      email
+      major
+      booked
+      mostUsedLibrary
+      mostUsedTable
+      reservations
+      extensions
+      strikes
+    }
+  }
+`;
 
 export default function TabThreeScreen({
   navigation,
 }: RootTabScreenProps<'TabThree'>) {
   const isFocused = useIsFocused();
+  const [hasQR, setHasQR] = useState<boolean>(false);
+  const [tableIdentifier, setTableIdentifier] = useState<
+    string | null | undefined
+  >(null);
+
+  async function userHasQR() {
+    const identifier = await getIdentifier();
+
+    if (identifier !== null) {
+      setTableIdentifier(identifier);
+      setHasQR(true);
+    } else setHasQR(false);
+
+    return hasQR;
+  }
 
   useEffect(() => {
     getLibrary;
-  }, [isFocused]);
+    userHasQR();
+  }, [isFocused, hasQR]);
+
+  const tableData = useQuery(getTable, {
+    variables: {
+      identifier: tableIdentifier,
+    },
+  });
+  const userData = useQuery(getCurrentUser);
+
+  if (tableData.loading || userData.loading) {
+    return (
+      <View
+        style={[
+          containerStyle.container,
+          { justifyContent: 'center', alignItems: 'center' },
+        ]}
+      >
+        <ManropeText bold={true}>
+          Lädt... Bitte warten Sie einen Moment.
+        </ManropeText>
+      </View>
+    );
+  }
+
+  const userId = userData.data.getCurrentUser.id;
 
   return (
     <>
@@ -43,20 +117,16 @@ export default function TabThreeScreen({
           )}
         </UpperBody>
         {hasQR ? (
-          <View
-            style={[
-              bodyContainerStyle.container,
-              {
-                alignItems: hasQR ? 'flex-start' : 'center',
-                justifyContent: hasQR ? 'flex-start' : 'center',
-              },
-            ]}
-          ></View>
+          <QRContainer
+            table={tableData.data.getTable}
+            userId={userId}
+            tableId={tableIdentifier}
+          />
         ) : (
           <QRCodeIcon height={500} width={300} />
         )}
         {hasQR && (
-          <Button backgroundColor={purple100}>
+          <Button>
             <ManropeText bold={true} style={{ color: white }}>
               Verlängern
             </ManropeText>
