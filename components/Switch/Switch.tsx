86c8b0b6985a04a0d21d886cdf9143656e1be2ed
furkandmrblk <ibrahimdmrblk@outlook.de>
2021-10-25 10:50:35 +0200

    ✨  - Switch Component

---
+++ components/Switch/Switch.tsx
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+import { View, Switch, StyleSheet } from 'react-native';
+import { peach100, purple100 } from '../../constants/Colors';
+
+type ToggleProps = {
+  toggle: boolean;
+  setToggle: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+export const Toggle = ({ toggle, setToggle }: ToggleProps) => {
+  const toggleSwitch = () => setToggle((prevState) => !prevState);
+
+  return (
+    <Switch
+      trackColor={{ false: `${peach100}`, true: `${purple100}` }}
+      thumbColor={toggle ? `#fff` : `#000`}
+      ios_backgroundColor="#999"
+      onValueChange={toggleSwitch}
+      value={toggle}
+    />
+  );
+};
